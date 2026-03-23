a771096c4038922a6ae4c8967e6849d8cc5c85c5
Maria <dudahpereira13@gmail.com>
2026-03-23 15:49:25 -0300

    repository criado

---
+++ src/main/java/com/example/fornecedor/repositories/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.example.fornecedor.repositories;
+
+import com.example.fornecedor.models.FornecedorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<FornecedorModel, Long> {
+}
